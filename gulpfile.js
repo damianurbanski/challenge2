f2f370e3f840f042c15d023457e9b4e46bf95026
Damian <damianurbanski97@gmail.com>
2018-08-10 15:10:31 +0200

    Init
    initial files and logo

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+cleanCSS = require("gulp-clean-css"),
+uglify = require("gulp-uglify"),
+concat = require("gulp-concat"),
+rename = require("gulp-rename");
+
+gulp.task('css', () => {
+	return gulp.src(['./css/**/*.css', '!./css/**/*.min.css'])
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+     .pipe(rename({
+     	suffix: '.min'
+     }))
+	.pipe(gulp.dest('./css'))
+});
+
+gulp.task('js', () => {
+	return gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+	.pipe(uglify())
+	     .pipe(rename({
+     	suffix: '.min'
+     }))
+	.pipe(gulp.dest('./js'))
+});
+
+gulp.task('watch', () => {
+  gulp.watch(['./css/**/*.css', '!./css/**/*.min.css'], ['css']);
+  gulp.watch(['js/**/*.js', '!js/**/*.min.js'], ['js']);
+});
+
+
+gulp.task('default', ['css','js','watch']);
